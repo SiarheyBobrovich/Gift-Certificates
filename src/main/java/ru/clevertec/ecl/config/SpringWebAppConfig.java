148fb45ea6c0b288f5352d75ce704cab7568b941
Bobrovich Siarhey <siarhey.babrovich@gmail.com>
2023-04-19 04:03:42 +0300

    first commit

---
+++ src/main/java/ru/clevertec/ecl/config/SpringWebAppConfig.java
@@ -0,0 +1,11 @@
+package ru.clevertec.ecl.config;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"ru.clevertec.ecl"})
+public class SpringWebAppConfig {
+
+}
